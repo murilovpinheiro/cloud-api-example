aaf7be007a6200eea0330f97f841cd502cb2ca98
havillonf <havillonbfreitas@gmail.com>
2024-05-28 11:26:53 -0300

    minor fixes

--- src/main/resources/migration.sql
+++ src/main/resources/migration.sql
@@ -3,5 +3,5 @@ CREATE TABLE movies (
                         title VARCHAR(150) NOT NULL,
                         release_year integer NOT NULL,
                         description TEXT NOT NULL,
-                        image VARCHAR(50) NOT NULL
+                        image VARCHAR(250) NOT NULL
 );
