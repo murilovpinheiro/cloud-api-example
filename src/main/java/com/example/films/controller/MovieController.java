37e95aa35230c4e5dce8a2db8be27132935d8a86
havillonf <havillonbfreitas@gmail.com>
2024-06-03 17:45:48 -0300

    dockerfile and dynamo connection

--- src/main/java/com/example/films/controller/MovieController.java
+++ src/main/java/com/example/films/controller/MovieController.java
@@ -1,15 +1,22 @@
 package com.example.films.controller;
 
-import com.example.films.movie.Movie;
-import com.example.films.movie.MovieRequestDTO;
-import com.example.films.movie.MovieResponseDTO;
-import com.example.films.movie.MovieRepository;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.example.films.movie.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.List;
 
@@ -19,6 +26,12 @@ public class MovieController {
 
     @Autowired
     private MovieRepository repository;
+    String tableName = "dynamo-cloud";
+
+    AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.standard()
+            .withRegion(Regions.US_EAST_1)
+            .withCredentials(new EnvironmentVariableCredentialsProvider())
+            .build();
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
@@ -26,21 +39,29 @@ public class MovieController {
         try {
             Movie movie = new Movie(data);
             repository.save(movie);
+
+            Map<String, AttributeValue> item = new HashMap<>();
+            item.put("key", new AttributeValue(movie.getId().toString()));
+            item.put("title", new AttributeValue(movie.getTitle()));
+            item.put("action",  new AttributeValue("Insertion"));
+            item.put("dateTime", new AttributeValue(LocalDateTime.now().toString()));
+
+
+            // Executando a operação de inserção
+            var response = dynamoDB.putItem(tableName, item);
+
+            // Verificando se a inserção foi bem-sucedida
+            if (response.getSdkHttpMetadata().getHttpStatusCode() == HttpStatus.OK.value()) {
+                System.out.println("Item adicionado com sucesso!");
+            } else {
+                System.err.println("Erro ao adicionar o item.");
+            }
+
             return ResponseEntity.ok("Movie saved successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save movie");
         }
     }
-
-    /* Old Save
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping
-    public void saveMovie(@RequestBody MovieRequestDTO data){
-        Movie foodData = new Movie(data);
-        repository.save(foodData);
-        return;
-    }
-    */
 
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -57,6 +78,21 @@ public class MovieController {
         Optional<Movie> movieOptional = repository.findById(id);
         if (movieOptional.isPresent()) {
             repository.deleteById(id);
+            Map<String, AttributeValue> item = new HashMap<>();
+            item.put("key", new AttributeValue(movieOptional.get().getId().toString()));
+            item.put("title", new AttributeValue(movieOptional.get().getTitle()));
+            item.put("action",  new AttributeValue("Deletion"));
+            item.put("dateTime", new AttributeValue(LocalDateTime.now().toString()));
+
+            // Executando a operação de inserção
+            var response = dynamoDB.putItem(tableName, item);
+
+            // Verificando se a inserção foi bem-sucedida
+            if (response.getSdkHttpMetadata().getHttpStatusCode() == HttpStatus.OK.value()) {
+                System.out.println("Item adicionado com sucesso!");
+            } else {
+                System.err.println("Erro ao adicionar o item.");
+            }
             return ResponseEntity.ok("Movie deleted successfully");
         } else {
             return ResponseEntity.notFound().build();
--- src/main/resources/db/migration/movies/V1__init-movies.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE movies (
-                        id SERIAL PRIMARY KEY,
-                        title VARCHAR(150) NOT NULL,
-                        release_year integer NOT NULL,
-                        description TEXT NOT NULL,
-                        image VARCHAR(250) NOT NULL
-);
